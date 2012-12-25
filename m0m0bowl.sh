9df95954dcf823931d501958e39c581071ebf40b
Edward Pham <edpham@gmail.com>
2012-12-24 23:46:39 -0800

    Adding a shell script for UNIX/Mac users and updating the sample data files

---
+++ m0m0bowl.sh
@@ -0,0 +1,3 @@
+python processDraft.py $1_raw_draftresults.txt $1_processed_draftresults.txt $2
+python processWaivers.py $1_processed_draftresults.txt $1_raw_waiverwireresults.txt $1_processed_waivermoves.txt
+python processCompleteRosters.py $1_processed_waivermoves.txt $1_raw_currentrosters.txt > $1_processed_currentrosters.txt
